008e924cd65b8ae49ddc7bb6cd13bab220165240
Bryan-us-dev <bryanweb82@gmail.com>
2019-11-15 13:27:13 +0530

    changes

--- ecommerce-front/src/App.js
+++ ecommerce-front/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
 
-const App = ()=> <div>Hello from React</div>
+const App = () => <div>Hello from React</div>;
+
 export default App;
--- ecommerce-front/src/user/Signup.js
+++ ecommerce-front/src/user/Signup.js
@@ -1,34 +1,30 @@
-import React,{useState} from "react";
-import Layout from "../core/Layout"; 
-import {Link} from "react-router-dom"; 
-import {signup} from "../auth";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Layout from '../core/Layout';
+import { signup } from '../auth';
 
-const Signup = ()=> {
-    const [values,setValues] = useState({
-        name:'',
-        email:'',
-        password:'',
-        error:'',
+const Signup = () => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
         success: false
     });
-    
-    const {name,email,password , success , error } = values;
 
-    const handleChange = name=> event=> {
-        setValues({...values, error: false,[name]: event.target.value})
-    }  
-       
+    const { name, email, password, success, error } = values;
 
-    const clickSubmit = (event)=>{
-      
-        event.preventDefault() 
-        setValues({...values,error: false})
-        signup({name,email,password})
-        .then(data=>{
-            if(data.error){
-                setValues({...values, error: data.error, success: false})
-            } 
-            else{
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false });
+        signup({ name, email, password }).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, success: false });
+            } else {
                 setValues({
                     ...values,
                     name: '',
@@ -36,51 +32,55 @@ const Signup = ()=> {
                     password: '',
                     error: '',
                     success: true
-                })
+                });
             }
-        })
-    }
+        });
+    };
 
-    const signUpForm = ()=>(
+    const signUpForm = () => (
         <form>
             <div className="form-group">
-               <label  className="text-">Name</label> 
-               <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
-            </div>  
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
+            </div>
 
             <div className="form-group">
-               <label  className="text-">Email</label> 
-               <input onChange={handleChange('email')}type="email" className="form-control"value={email}/>
-            </div>  
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email')} type="email" className="form-control" value={email} />
+            </div>
 
             <div className="form-group">
-               <label  className="text-">Password</label> 
-               <input onChange={handleChange('password')} type="password" className="form-control" value={password}/>
-            </div> 
-            <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+                <label className="text-muted">Password</label>
+                <input onChange={handleChange('password')} type="password" className="form-control" value={password} />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+            </button>
         </form>
-    ); 
-       
-    const showError =()=>(
-        <div className="alert alert-danger" style={{display: error?'':'none'}}>
+    );
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
             {error}
         </div>
-    );  
+    );
 
-    const showSuccess =()=>(
-        <div className="alert alert-info" style={{display: success?'':'none'}}>
-            New Account is created.Please <Link to="/signin">Signin</Link>  
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{ display: success ? '' : 'none' }}>
+            New account is created. Please <Link to="/signin">Signin</Link>
         </div>
-    ); 
+    );
 
     return (
-    <Layout title="Signup"
-     description="Signup to Node React-Ecommerce App" 
-     className="container col-md-8 offset-md-2"> 
-       {showError()}
-       {showSuccess()}
-       {signUpForm()}
-    </Layout>
+        <Layout
+            title="Signup"
+            description="Signup to Node React E-commerce App"
+            className="container col-md-8 offset-md-2"
+        >
+            {showSuccess()}
+            {showError()}
+            {signUpForm()}
+        </Layout>
     );
 };
 
