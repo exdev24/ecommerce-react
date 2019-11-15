23b6400b7b0e1da223e5773ddb57b4d28e736b0b
Bryan-us-dev <bryanweb82@gmail.com>
2019-11-15 13:24:38 +0530

    Changes

--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -1,60 +1,65 @@
-import React ,{useState,useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
-import { getProducts } from './apiCore'; 
+import { getProducts } from './apiCore';
 import Card from './Card';
 import Search from './Search';
 
-const Home = ()=> { 
-    const [productsBySell, setProductsBySell] = useState([])
-    const [productsByArrival, setProductsByArrival] = useState([])
-    const [error, setError] = useState(false) 
+const Home = () => {
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [error, setError] = useState(false);
 
-    const loadProductsBySell = ()=>{
-        getProducts('sold').then(data=>{
-            if(data.error){
-               setError(data.error)
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if (data.error) {
+                setError(data.error);
             } else {
-               setProductsBySell(data)
+                setProductsBySell(data);
             }
         });
-    };  
-    
-    const loadProductsByArrival = ()=>{
-        getProducts('createdAt').then(data=>{
-            if(data.error){
-               setError(data.error)
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            console.log(data);
+            if (data.error) {
+                setError(data.error);
             } else {
-               setProductsByArrival(data)
+                setProductsByArrival(data);
             }
         });
-    };  
+    };
 
-    useEffect(()=>{
-       loadProductsByArrival();
-       loadProductsBySell();
-
-    },[]);
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
 
     return (
-      <Layout title="Home Page" description="Node React E-commerce App" className="container-fluid"> 
-     <Search />
-       <h2 className="mb-4">New Arrivals</h2>
-          <div className="row"> 
-        {  productsByArrival.map((product,i)=>(
-            <div Key={i} className="col-4 mb-3">
-                <Card  product={product} />
+        <Layout
+            title="FullStack React Node MongoDB Ecommerce App"
+            description="Node React E-commerce App"
+            className="container-fluid"
+        >
+            <Search />
+            <h2 className="mb-4">New Arrivals</h2>
+            <div className="row">
+                {productsByArrival.map((product, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card product={product} />
+                    </div>
+                ))}
             </div>
-        ))}
-          </div> 
-          <h2 className="mb-4">Best Sellers</h2> 
-          <div className="row">
-        {  productsBySell.map((product,i)=>(
-             <div Key={i} className="col-4 mb-3">
-             <Card  product={product} />
-             </div>  
-                   ))}     
-          </div> 
-      </Layout>    
+
+            <h2 className="mb-4">Best Sellers</h2>
+            <div className="row">
+                {productsBySell.map((product, i) => (
+                    <div key={i} className="col-4 mb-3">
+                        <Card product={product} />
+                    </div>
+                ))}
+            </div>
+        </Layout>
     );
 };
 
--- ecommerce-front/src/core/fixedPrices.js
+++ ecommerce-front/src/core/fixedPrices.js
@@ -1,4 +1,4 @@
-export const  prices = [
+export const prices = [
     {
         _id: 0,
         name: "Any",
@@ -7,27 +7,26 @@ export const  prices = [
     {
         _id: 1,
         name: "$0 to $9",
-        array: [0,9]
+        array: [0, 9]
     },
     {
         _id: 2,
         name: "$10 to $19",
-        array: [10,19]
+        array: [10, 19]
     },
     {
         _id: 3,
         name: "$20 to $29",
-        array: [20,29]
+        array: [20, 29]
     },
     {
         _id: 4,
         name: "$30 to $39",
-        array: [30,39]
+        array: [20, 29]
     },
-    ,
     {
         _id: 5,
-        name: "$More than 40",
-        array: [40,99]
-    }    
-]
+        name: "More than $40",
+        array: [40, 99]
+    }
+];
